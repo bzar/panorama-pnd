6c79b4ac808feae8721becadeefedebf3598f16c
Teemu Erkkola <teemu.erkkola@iki.fi>
2011-09-20 18:02:55 +0300

    Updated libmilky

--- PND_content/scripts/panorama.sh
+++ PND_content/scripts/panorama.sh
@@ -3,7 +3,8 @@ export PATH=":${PATH:-"/usr/bin:/bin:/usr/local/bin"}"
 export LD_LIBRARY_PATH="lib:${LD_LIBRARY_PATH:-"/usr/lib:/lib"}"
 export QML_IMPORT_PATH=$(pwd)/imports
 export QT_PLUGIN_PATH=$(pwd)/qt/plugins
-
+export LC_NUMERIC=en_US.utf8
+export XDG_DATA_DIRS=$(pwd)/"share:${XDG_DATA_DIRS:-"/usr/share"}"
 if [ ! -d ./interfaces ]; then
 	cp -R interfaces_default interfaces
 fi
